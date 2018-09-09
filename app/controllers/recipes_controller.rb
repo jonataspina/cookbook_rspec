7ab5b4d29c860a05186582b8c1f1955d646baf74
Jônatas Pina <jonatas.pina@vagas.com.br>
2018-09-08 23:30:31 -0300

    Usuário edita uma receita

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,4 +19,21 @@ class RecipesController < ApplicationController
         # redirecionar para a receita recém criada.
         redirect_to recipe_path recipe.id
     end 
+
+    def edit
+        id = params[:id]
+        @recipe = Recipe.find(id)
+    end
+
+    def update 
+        id = params[:id]
+        @recipe = Recipe.find(id)
+
+        recipe_params = params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty,
+        :cook_time, :ingredients, :cook_method)
+
+        @recipe.update(recipe_params)
+
+        redirect_to recipe_path @recipe
+    end 
 end
