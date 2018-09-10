42aeb92ce18eb32ea2ff7170f29ac0ecc33187a5
Jônatas Pina <jonatas.pina@vagas.com.br>
2018-09-10 00:56:48 -0300

    Usuário marca receita como 'destaque'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
     def welcome
-        @recipes = Recipe.all  
+        #@recipes = Recipe.all
+        @featured_recipes = Recipe.where(featured: true)
+        @other_recipes = Recipe.where(featured: false)  
     end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_165106) do
+ActiveRecord::Schema.define(version: 2018_09_10_022938) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_06_165106) do
     t.datetime "updated_at", null: false
     t.text "ingredients"
     t.text "cook_method"
+    t.boolean "featured", default: true
   end
 
 end
